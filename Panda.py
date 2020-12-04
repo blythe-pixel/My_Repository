2a21fc12ebfebe75d3f33d64c719ff09e77a116b
Kristian Roger M. Agdeppa <kristianrogeragdeppa@gmail.com>
2020-12-04 16:39:09 +0800

    PYTHON PANDAS

--- Panda.py
+++ Panda.py
@@ -1,4 +1,4 @@
-# TODO PRINTING DATA FROM CVS FILE
+# TODO PRINTING DATA FROM CSV FILE
 # from pandas import *
 #
 # df = read_csv(r'C:\Users\User\PycharmProjects\My_Repository\Pokemon Data Frame\pokemon_data.csv')
@@ -15,7 +15,6 @@
 # from pandas import *
 #
 # df = read_csv(r'C:\Users\User\PycharmProjects\My_Repository\Pokemon Data Frame\pokemon_data.csv')
-# df = df.head(722)
 # print(df.to_string())
 #
 # """ printing columns by name"""
@@ -39,21 +38,62 @@
 # TODO DATA SORTING
 # from pandas import *
 #
-# df = read_excel('Students_DataFrame.xlsx')
+# df = read_excel('Students_DataFrame(Modified).xlsx')
 #
 # print(df.sort_values(by=['Name'], ascending=[True]).to_string())
+#
+# """ it sorts the values inside the file, you can use this by df.sort_values()
+#     by= means the name of the column you want to sort, and you can specify
+#     in the ascending= if it is sorted in reverse or not. Note: it can only filter
+#     one column per function. """
 
 # TODO MAKE CHANGES AND UPDATING THE DATAFRAME
 # from pandas import *
 # import datetime
 # x = datetime.datetime.now()
 # df = read_excel('Students_DataFrame.xlsx')
-# 
+#
 # df['GPA'] = df['Programming 101'] + df['Introduction to Computer Science']
 # df['GPA'] = df['GPA'] / 2
 #
 # df['Date Modified'] = x
 # df.to_excel(f'Students_DataFrame(Modified).xlsx', index=False)
+#
+# """ In order to update the current DataFrame, you need to use the function
+#     df.to_excel() which the takes the first argument as the name of the file
+#     and the second argument which is index=False implies that no index value
+#     will be placed in the current file that will be created"""
+#
+# """ if you intend to create a txt file from a cvs (comma separated values) file
+#     you can put an argument which is sep="\t" which indicates that the file
+#     is separated by tabs after it is created"""
 
-
-
+# TODO DATA FILTRATION (based on multiple conditions)
+# from pandas import *
+#
+# df = read_excel(r'C:\Users\User\PycharmProjects\My_Repository\Pokemon Data Frame\pokemon_data.xlsx')
+# print("Legendary Pokemon (Phychic)")
+# print(df.loc[(df['Legendary'] == True) & (df['Type 1'] == 'Psychic')].to_string())
+# print()
+# print("Legendary Pokemon (Grass)")
+# print(df.loc[(df['Legendary'] == True) & (df['Type 1'] == 'Grass')].to_string())
+# print()
+# print("Legendary Pokemon (Ice)")
+# print(df.loc[(df['Legendary'] == True) & (df['Type 1'] == 'Ice')].to_string())
+# print()
+# print("Legendary Pokemon (Dragon)")
+# print(df.loc[(df['Legendary'] == True) & (df['Type 1'] == 'Dragon') & (df['Attack'] >= 100)
+#              & (df['Defense'] >= 100)].to_string())
+#
+# """ you can use || to that represents as 'or' and & to represent as 'and' """
+#
+# """ Data filtering is one of the useful things in Pandas, you can use this by
+#     by the use of df.loc[], and the name of the column you want to filter and
+#     it will automatically ask for an argument. Note: if the word you want to
+#     filter is named "True", don't put a string quotation. """
+#
+# filtered_data = df.loc[(df['Legendary'] == True) & (df['Type 1'] == 'Dragon') & (df['Attack'] >= 100)
+#                        & (df['Defense'] >= 100)]
+#
+# filtered_data.to_excel("filtered_data.xlsx", index=False)
+# """ You can also save your filtered data into any file you want (xlsx, csv, txt, etc.) """
